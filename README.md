c4dfc2e2a3c235be821b7c5c40dbb1e194e1e25b
Guix <guimega.gh@gmail.com>
2025-06-07 10:45:37 -0400

    chore: app init

---
+++ README.md
@@ -0,0 +1,3 @@
+# news_responsive_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/news_responsive_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.news_responsive_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
